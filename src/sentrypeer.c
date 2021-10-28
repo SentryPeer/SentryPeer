3448b1b4e49e3e8af4c535d2e8ed22ff48316af6
Gavin Henry <ghenry@sentrypeer.org>
2021-10-28 21:42:12 +0100

    Funding and package vars in configure.ac
    Signed-off-by: Gavin Henry <ghenry@sentrypeer.org>

--- src/sentrypeer.c
+++ src/sentrypeer.c
@@ -1,15 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only  */
 /* Copyright (c) 2021 Gavin Henry <ghenry@sentrypeer.org> */
 
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "sentrypeer.h"
 
 int main(void) {
 
-  fprintf(stdout, "Hello from %s, version: %s\n", SENTRYPEER_NAME,
-          SENTRYPEER_VERSION);
+  fprintf(stdout, "Hello from '%s'\nversion: %s\nrevision: %s \n", PACKAGE_NAME,
+          PACKAGE_VERSION, REVISION);
 
   return EXIT_SUCCESS;
 }
