19835d56523999e8525e60ab2be238bdaecee674
Gavin Henry <ghenry@sentrypeer.org>
2022-03-07 21:08:52 +0000

    Don't drop const

--- src/utils.c
+++ src/utils.c
@@ -108,13 +108,15 @@ char *util_addr_string(const struct sockaddr *addr)
 	char *s = NULL;
 	switch (addr->sa_family) {
 	case AF_INET: {
-		struct sockaddr_in *addr_in = (struct sockaddr_in *)addr;
+		const struct sockaddr_in *addr_in =
+			(const struct sockaddr_in *)addr;
 		s = malloc(INET_ADDRSTRLEN);
 		inet_ntop(AF_INET, &(addr_in->sin_addr), s, INET_ADDRSTRLEN);
 		break;
 	}
 	case AF_INET6: {
-		struct sockaddr_in6 *addr_in6 = (struct sockaddr_in6 *)addr;
+		const struct sockaddr_in6 *addr_in6 =
+			(const struct sockaddr_in6 *)addr;
 		s = malloc(INET6_ADDRSTRLEN);
 		inet_ntop(AF_INET6, &(addr_in6->sin6_addr), s,
 			  INET6_ADDRSTRLEN);
