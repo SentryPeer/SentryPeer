f87c09b3d2c0f6a1de7b2704b741f956122de679
Gavin Henry <ghenry@sentrypeer.org>
2024-05-14 16:31:54 +0100

    Playing with rustls and cbindgen.

---
+++ src/sentrypeer_rust.h
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only  */
+/* Copyright (c) 2021 - 2024 Gavin Henry <ghenry@sentrypeer.org> */
+/*
+   _____            _              _____
+  / ____|          | |            |  __ \
+ | (___   ___ _ __ | |_ _ __ _   _| |__) |__  ___ _ __
+  \___ \ / _ \ '_ \| __| '__| | | |  ___/ _ \/ _ \ '__|
+  ____) |  __/ | | | |_| |  | |_| | |  |  __/  __/ |
+ |_____/ \___|_| |_|\__|_|   \__, |_|   \___|\___|_|
+                              __/ |
+                             |___/
+*/
+
+
+#ifndef SENTRYPEER_RUST_H
+#define SENTRYPEER_RUST_H
+
+/* Generated with cbindgen:0.26.0 */
+
+/* Warning, do not modify this manually. */
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/**
+ * The simplest function used to confirm that calling our Rust library from C is working
+ */
+void display_rust(void);
+
+#endif /* SENTRYPEER_RUST_H */
