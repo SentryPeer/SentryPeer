b2762746f2b53d4980052acb58c6065fef3cbe08
Gavin Henry <ghenry@sentrypeer.org>
2024-08-28 11:50:55 +0100

    Free CStrings on Rust side as created there.

--- sentrypeer_rust/src/tls.rs
+++ sentrypeer_rust/src/tls.rs
@@ -14,6 +14,7 @@ use std::ffi::CString;
 use std::fs::File;
 use std::io::{self, BufReader, ErrorKind};
 use std::net::ToSocketAddrs;
+use std::os::raw::c_char;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
@@ -30,7 +31,7 @@ use tokio::net::TcpListener;
 use tokio_rustls::{rustls, TlsAcceptor};
 
 // Our C FFI functions
-use crate::{sentrypeer_config, sip_log_event, sip_message_event_destroy, sip_message_event_new};
+use crate::{sentrypeer_config, sip_log_event, sip_message_event_new};
 
 struct SentryPeerConfig {
     p: *mut sentrypeer_config,
@@ -80,43 +81,72 @@ fn log_sip_packet(
     let mut peer_addr_c: OsSocketAddr = peer_addr.into();
     let sentrypeer_c_config = sentrypeer_c_config.p;
 
+    // To free on our side
+    // https://doc.rust-lang.org/std/ffi/struct.CString.html#method.into_raw
+    let packet_ptr = CString::new(String::from_utf8_lossy(&buf[..bytes_read]).to_string())
+        .unwrap()
+        .into_raw();
+    let transport_type_ptr = CString::new("TLS").unwrap().into_raw();
+    let client_ip_addr_ptr = CString::new(peer_addr.to_string()).unwrap().into_raw();
+    let dest_ip_addr_ptr = CString::new(listen_addr.to_string()).unwrap().into_raw();
+
     unsafe {
         // https://doc.rust-lang.org/std/primitive.pointer.html
-        let mut sip_message = sip_message_event_new(
+        let sip_message = sip_message_event_new(
             // packet from stream
-            CString::new(String::from_utf8_lossy(&buf[..bytes_read]).to_string())
-                .unwrap()
-                .into_raw(),
+            packet_ptr,
             // packet length
             bytes_read,
             // socket (can be anything)
             c_int::from(0),
             // transport_type
-            CString::new("TLS").unwrap().into_raw(),
+            transport_type_ptr,
             // client_ip_addr
             peer_addr_c.as_mut_ptr() as *mut sockaddr,
             // client_ip_addr_str
-            CString::new(peer_addr.to_string()).unwrap().into_raw(),
+            client_ip_addr_ptr,
             // client_ip_addr_len
             peer_addr_c.len().try_into().unwrap(),
             // dest_ip_addr_str
-            CString::new(listen_addr.to_string()).unwrap().into_raw(),
+            dest_ip_addr_ptr,
         );
 
         if sip_log_event(sentrypeer_c_config, sip_message) != libc::EXIT_SUCCESS {
             eprintln!("Failed to log SIP message event");
 
             // Clean up
-            sip_message_event_destroy(&mut sip_message);
+            clean_up_sip_message(
+                packet_ptr,
+                transport_type_ptr,
+                client_ip_addr_ptr,
+                dest_ip_addr_ptr,
+            );
 
             return libc::EXIT_FAILURE;
         }
 
         // Clean up
-        sip_message_event_destroy(&mut sip_message);
+        clean_up_sip_message(
+            packet_ptr,
+            transport_type_ptr,
+            client_ip_addr_ptr,
+            dest_ip_addr_ptr,
+        );
 
         libc::EXIT_SUCCESS
     }
+}
+
+unsafe fn clean_up_sip_message(
+    packet_ptr: *mut c_char,
+    transport_type_ptr: *mut c_char,
+    client_ip_addr_ptr: *mut c_char,
+    dest_ip_addr_ptr: *mut c_char,
+) {
+    let _ = CString::from_raw(packet_ptr);
+    let _ = CString::from_raw(transport_type_ptr);
+    let _ = CString::from_raw(client_ip_addr_ptr);
+    let _ = CString::from_raw(dest_ip_addr_ptr);
 }
 
 #[tokio::main]
