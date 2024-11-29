9208d8af5ae4ee524faa4ef8fd725b06efcfbb5e
Gavin Henry <ghenry@sentrypeer.org>
2024-11-29 12:42:26 +0000

    TLS integration test.

--- sentrypeer_rust/tests/tls.rs
+++ sentrypeer_rust/tests/tls.rs
@@ -1,20 +1,66 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only  */
+/* Copyright (c) 2021 - 2024 Gavin Henry <ghenry@sentrypeer.org> */
+/*
+   _____            _              _____
+  / ____|          | |            |  __ \
+ | (___   ___ _ __ | |_ _ __ _   _| |__) |__  ___ _ __
+  \___ \ / _ \ '_ \| __| '__| | | |  ___/ _ \/ _ \ '__|
+  ____) |  __/ | | | |_| |  | |_| | |  |  __/  __/ |
+ |_____/ \___|_| |_|\__|_|   \__, |_|   \___|\___|_|
+                              __/ |
+                             |___/
+*/
 use assert_cmd::Command;
 use predicates::prelude::*;
 
 #[test]
 #[ignore = "not yet implemented"]
 fn tls_probe() {
-    let mut cmd = Command::new("../sentrypeer");
-    // Enable debug mode, responsive SIP mode, and JSON logging using the default JSON
-    // log file
-    cmd.arg("-drj");
+    // Delete our JSON log file
+    let mut json_log = Command::new("rm");
+    json_log
+        .args(["-f", "./sentrypeer_json.log"])
+        .assert()
+        .success();
 
-    // Debug mode will show the transport type which is TLS
-    cmd.assert()
+    // Start sentrypeer in the background
+    let mut cmd = std::process::Command::new("../sentrypeer");
+    // Enable debug mode, responsive SIP mode, our certs and JSON logging using the
+    // default JSON log file
+    cmd.arg("-drj");
+    cmd.args(["-t", "../tests/unit_tests/127.0.0.1.pem"]);
+    cmd.args(["-k", "../tests/unit_tests/127.0.0.1-key.pem"]);
+    cmd.args(["-z", "127.0.0.1:5061"]);
+
+    let mut sentrypeer = cmd.spawn().expect("Failed to start sentrypeer");
+
+    // Give sentrypeer time to start
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    // Run our SIP tester
+    let mut sipexer_cmd = Command::new("../tests/tools/sipexer");
+    sipexer_cmd.arg("-tls-insecure").arg("tls:127.0.0.1:5061");
+    sipexer_cmd.assert().failure();
+
+    // Check that the JSON log file has been created
+    let mut json_log = Command::new("cat");
+    json_log.arg("../sentrypeer_json.log");
+    json_log
+        .assert()
         .success()
         .stdout(predicate::str::contains("TLS"));
 
-    let mut sipexer_cmd = Command::new("../tests/tools/sipexer");
-    sipexer_cmd.arg("-tls-insecure").arg("tls:127.0.0.1:5061");
-    sipexer_cmd.assert().success();
+    // Kill sentrypeer
+    let _ = sentrypeer.kill();
+
+    // Delete our JSON log file again
+    let mut json_log = Command::new("rm");
+    json_log
+        .args(["-f", "./sentrypeer_json.log"])
+        .assert()
+        .success();
+
+    // Delete our sentrypeer.db file
+    let mut db = Command::new("rm");
+    db.args(["-f", "./sentrypeer.db"]).assert().success();
 }
