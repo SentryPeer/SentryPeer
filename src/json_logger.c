24560e1cfb368409e5105ef9af9da09e881a3bcc
Gavin Henry <ghenry@sentrypeer.org>
2025-03-31 15:15:28 +0100

    Check return code of json_http_post_bad_actor()

--- src/json_logger.c
+++ src/json_logger.c
@@ -625,9 +625,21 @@ int json_http_post_bad_actor(sentrypeer_config *config,
 					config->oauth2_access_token = 0;
 				}
 				if (json_http_post_bad_actor(
-					    config, bad_actor_to_log)) {
+					    config, bad_actor_to_log) !=
+				    EXIT_SUCCESS) {
+					{
+						fprintf(stderr,
+							"Failed to POST bad actor.\n");
+
+						return EXIT_FAILURE;
+					}
+				} else {
 					fprintf(stderr,
-						"Failed to POST bad actor.\n");
+						"WebHook POSTing failed: HTTP response code %ld\n",
+						http_response_code);
+
+					free(json_string);
+					http_cleanup_curl(curl, headers);
 
 					return EXIT_FAILURE;
 				}
@@ -641,25 +653,15 @@ int json_http_post_bad_actor(sentrypeer_config *config,
 
 				return EXIT_FAILURE;
 			}
-		} else {
-			fprintf(stderr,
-				"WebHook POSTing failed: HTTP response code %ld\n",
-				http_response_code);
-
-			free(json_string);
-			http_cleanup_curl(curl, headers);
-
-			return EXIT_FAILURE;
 		}
-	}
 
-	free(json_string);
-	http_cleanup_curl(curl, headers);
+		free(json_string);
+		http_cleanup_curl(curl, headers);
 
-	if (config->debug_mode || config->verbose_mode) {
-		fprintf(stderr,
-			"WebHook POSTing succeeded: HTTP response code %ld\n",
-			http_response_code);
+		if (config->debug_mode || config->verbose_mode) {
+			fprintf(stderr,
+				"WebHook POSTing succeeded: HTTP response code %ld\n",
+				http_response_code);
+		}
+		return EXIT_SUCCESS;
 	}
-	return EXIT_SUCCESS;
-}
